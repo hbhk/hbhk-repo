87126626159c9148b427c72df85519521cf9162b
hbhk <1024784402@qq.com>
2014-08-04 05:05:11 -0700

    Maven artifacts for 0.0.1-SNAPSHOT

---
+++ src/test/java/foo/AppTest.java
@@ -0,0 +1,27 @@
+package foo;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+
+{
+	/**
+	 * Create the test case
+	 * 
+	 * @param testName
+	 *            name of the test case
+	 */
+	public AppTest(String testName) {
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+	}
+}
